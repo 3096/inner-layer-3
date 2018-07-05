42002dd8a17eb4480bc913d7f8c95722a4b3cfb1
3096 <f.dysprosium@gmail.com>
2018-07-05 15:29:21 -0700

    Abandon template ValueNode approach, seems unnecessary.

--- BymlNodes.h
+++ BymlNodes.h
@@ -1,6 +1,7 @@
 #include <stdint.h>
 #include <string>
 #include <cstring>
+#include <unordered_map>
 
 const uint16_t LE_BYML_MAGIC = 0x4259; // hex for "YB"
 
@@ -23,9 +24,8 @@ typedef struct {
 } BymlHeader;
 
 class OutOfBoundsException {};
-
 class NodeNotFoundException {};
-
+class NodeTypeMismatch {};
 class IHaventImplementedThisYetException {};
 
 class Node
@@ -33,8 +33,11 @@ class Node
 protected:
     uint8_t* start;
 public:
+    Node() {};
     Node(uint8_t* node_start);
-    ~Node();
+    virtual ~Node();
+
+    uint8_t* getStart() {return start;}
 };
 
 Node::Node(uint8_t* node_start) {
@@ -42,8 +45,9 @@ Node::Node(uint8_t* node_start) {
 }
 
 Node::~Node() {
-
+    
 }
+
 
 class StringTabNode : public Node {
 private:
@@ -74,67 +78,76 @@ std::string StringTabNode::operator[](int i) {
 }
 
 
-template <typename T>
-class ValueNode : public Node {
-private:
-    int str_index = 0;
-    const uint8_t* type;
-    uint32_t* value;
-
-    StringTabNode* name_tab;
-
-public:
-    ValueNode(uint8_t* node_start, StringTabNode* name_tab_ptr);
-    ~ValueNode() {};
-
-    T& operator=(const T& x);
-
-    std::string name() {return (*name_tab)[str_index];}
-};
-
-template <typename T>
-ValueNode<T>::ValueNode(uint8_t* node_start, StringTabNode* name_tab_ptr):
-    Node(node_start), name_tab(name_tab_ptr) {
-    memcpy(&str_index, node_start, 3);
-    type = &start[3];
-    value = (uint32_t*)&start[4];
-};
-
-template <typename T>
-T& ValueNode<T>::operator=(const T& x) {
-    memcpy(value, x, sizeof(value));
-    return *(T*)value;
-}
-
-
+class ValueNode;
 
 class DictNode : public Node {
 private:
     int count = 0;
     StringTabNode* name_tab;
     bool nameTabNeedDelete = false;
+    uint8_t* file;
+
+    std::unordered_map<std::string, uint8_t*> ValueNodeMap;
+
+    void initializeMaps();
 
 public:
-    DictNode(uint8_t* node_start, uint8_t* str_tab_start);
-    DictNode(uint8_t* node_start, StringTabNode* name_tab_ptr);
+    DictNode(uint8_t* node_start, uint8_t* str_tab_start, uint8_t* file_ptr);
+    DictNode(ValueNode* value_ptr);
     ~DictNode();
 
-    Node operator[](int i);
-    Node operator[](std::string name);
+    ValueNode operator[](int i);
+    ValueNode operator[](std::string name);
 
     int size() {return count;}
     std::string getNodeNameByIndex(int i);
+    StringTabNode* getNameTab() {return name_tab;}
 };
 
-DictNode::DictNode(uint8_t* node_start, uint8_t* str_tab_start):
-    Node(node_start) {
-    memcpy(&count, &start[1], 3);
+
+DictNode::DictNode(uint8_t* node_start, uint8_t* str_tab_start, uint8_t* file_ptr):
+    Node(node_start), file(file_ptr) {
     name_tab = new StringTabNode(str_tab_start);
     nameTabNeedDelete = true;
+    memcpy(&count, &start[1], 3);
 }
 
-DictNode::DictNode(uint8_t* node_start, StringTabNode* name_tab_ptr):
-    Node(node_start), name_tab(name_tab_ptr) {
+class ValueNode : public Node {
+private:
+    int str_index = 0;
+    uint8_t type;
+    uint32_t* value;
+
+    DictNode* parentDict;
+
+public:
+    ValueNode(uint8_t* node_start, DictNode* parentDict);
+    ~ValueNode() {};
+
+    void setValue(const uint32_t& x);
+
+    uint32_t getValue() const {return *value;}
+    uint8_t getType() const {return type;}
+    DictNode* getParentDict() {return parentDict;}
+    std::string name() {return (*(parentDict->getNameTab()))[str_index];}
+};
+
+ValueNode::ValueNode(uint8_t* node_start, DictNode* parent_dict_ptr):
+    Node(node_start), parentDict(parent_dict_ptr) {
+    memcpy(&str_index, node_start, 3);
+    type = start[3];
+    value = (uint32_t*)&start[4];
+}
+
+DictNode::DictNode(ValueNode* value_node_ptr) {
+    if(value_node_ptr->getType() != DICT_NODE_TYPE) {
+        throw NodeTypeMismatch();
+    }
+    DictNode* parentDict = value_node_ptr->getParentDict();
+    name_tab = parentDict->name_tab;
+    
+    file = parentDict->file;
+    start = &file[value_node_ptr->getValue()];
     memcpy(&count, &start[1], 3);
 }
 
@@ -144,51 +157,27 @@ DictNode::~DictNode() {
     }
 }
 
-Node DictNode::operator[](int i) {
+void DictNode::initializeMaps() {
+    uint8_t* cur_node_start = start;
+    // to be implemented
+}
+
+ValueNode DictNode::operator[](int i) {
     if(i < 0 || i >= count)
         throw OutOfBoundsException();
-    uint8_t* node_start = &this->start[4 + i*4];
+    uint8_t* node_start = &this->start[4 + i*8];
     uint8_t type = node_start[3];
     
-    Node ret = NULL;
-    switch(type) {
-    case STR_NODE_TYPE:
-    case PATH_NODE_TYPE:
-        ret = ValueNode<uint32_t>(node_start, name_tab);
-        break;
-    case ARRAY_NODE_TYPE:
-        throw IHaventImplementedThisYetException();
-        break;
-    case DICT_NODE_TYPE:
-        ret = DictNode(node_start, name_tab);
-        break;
-    case STR_TAB_NODE_TYPE:
-        ret = StringTabNode(node_start);
-        break;
-    case PATH_TAB_NODE_TYPE:
-        throw IHaventImplementedThisYetException();
-        break;
-    case BOOL_NODE_TYPE:
-        ret = ValueNode<bool>(node_start, name_tab);
-        break;
-    case INT_NODE_TYPE:
-        ret = ValueNode<int>(node_start, name_tab);
-        break;
-    case FLOAT_NODE_TYPE:
-        ret = ValueNode<float>(node_start, name_tab);
-        break;
-    default:
-        throw IHaventImplementedThisYetException();
-    }
+    ValueNode ret = ValueNode(node_start, this);
 
     return ret;
 }
 
-Node DictNode::operator[](std::string name) {
+ValueNode DictNode::operator[](std::string name) {
     // just search one by one for now
     // will probably implement some sort of hashmap solution later
     for (int i = 0; i < count; i++) {
-        Node curNode = (*this)[i];
+        ValueNode curNode = (*this)[i];
         if(getNodeNameByIndex(i) == name) {
             return curNode;
         }
@@ -199,8 +188,14 @@ Node DictNode::operator[](std::string name) {
 std::string DictNode::getNodeNameByIndex(int i) {
     if(i < 0 || i >= count)
         throw OutOfBoundsException();
-    uint8_t* node_start = &this->start[4 + i*4];
+    uint8_t* node_start = &this->start[4 + i*8];
     int name_index = 0;
     memcpy(&name_index, node_start, 3);
     return (*name_tab)[name_index];
+}
+
+
+
+void ValueNode::setValue(const uint32_t& x) {
+    memcpy(value, &x, sizeof(value));
 }
